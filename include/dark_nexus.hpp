1e948986c5667151aed93e62cb50100462cfb9e1
fuckmrshl <fkmrshl@gmail.com>
2026-04-12 07:13:18 -0400

    dark_nexus.hpp

--- include/dark_nexus.hpp
+++ include/dark_nexus.hpp
@@ -107,4 +107,8 @@ void osint_scan(const std::string& username);
 void traceroute(const std::string& target);
 void full_recon(const std::string& ip);
 
-void subdomain_scan(const std::string& domain);
+void subdomain_scan(const std::string& domain, 
+                    const std::string& wordlist_path = "", 
+                    int max_threads = 150,
+                    bool run_permutations = true, 
+                    bool deep_passive = true);
